d011536bd2af70972cb57802442456b91978d5f2
lorena252 <lorenaavalos252@hotmail.com>
2023-10-17 15:14:39 -0300

    function add job

--- js/api.js
+++ js/api.js
@@ -1,8 +1,53 @@
-const getJobs = () =>{
-    fetch(`https://65271cad917d673fd76d6b9b.mockapi.io/api/jobs`)
-    .then((response) => response.json())
-    .then((data) => renderJobs(data))
+
+const getJobs = async () =>{
+    show("#spinner")
+ let response = await fetch(`https://65271cad917d673fd76d6b9b.mockapi.io/api/jobs`)
+ let data = await response.json()
+ renderJobs(data)
+console.log(data)
 }
+
+
+// POST NUEVO TRABAJO//
+
+const addJob = async (job) =>{
+  try {
+const response = await  fetch(`https://65271cad917d673fd76d6b9b.mockapi.io/api/jobs`,{
+    method: "POST",
+      headers: {"content-type" : "application/json"},
+    body: JSON.stringify(job),
+}
+)
+   const data = await response.json()
+   if(data){
+    getJobs()
+   }
+  }catch(error){
+ alert("error de ejecucion")
+  }
+}
+
+
+
+
+
+
+//FUNCION EDITAR TRABAJO//
+
+
+
+
+
+
+
+// FUNCION DELETE TRABAJO//
+
+
+
+
+// }
+
+
 
 
 
--- js/main.js
+++ js/main.js
@@ -5,46 +5,49 @@ const hide = (selector) => $(selector).classList.add("hidden");
 const show = (selector) => $(selector).classList.remove("hidden");
 const cleanContainer = (selector) => ($(selector).innerHTML = "");
 
-
+const randomId = () => self.crypto.randomUUID();
 
 const saveJob = () =>{
    let newJob = {
+      id : randomId(),
       name : $("#name-form").value,
-      image : "",
+      image : $("#image-form").value,
       description: $("#description-form").value,
       location: $("#location-form").value,
       category: $("#category-form").value,
       seniority: $("#señority-form").value,
-      // benefits: {
-      //   vacation: $("#vacation").value,
-      //     health_ensurance: $("health").value,
-      //     internet_paid: $("#internet").checked,
-      // },
-      // salary:  Number($("#salary-form").value),
+      benefits: {
+        vacation: $("#vacation-form").value,
+          health_ensurance: $("#health-form").value,
+          internet_paid: $("#internet-form").checked,
+       },
+      salary:  Number($("#salary-form").value),
       // long_term: $("long-term".checked),
       // languages: $("#languajes").value,
-  
+    
     }
-console.log(newJob)
+    console.log(newJob)
+addJob(newJob)
 }
 
 
 const renderJobs = (data) =>{
-   for (const {name,image,description,location,category,seniority} of data){
-      console.log(data)
+   for (const {id, name,image,description,location,category,seniority} of data){
+
      $("#containers").innerHTML += `
      <div class="card m-2   " id="containers-card" style="width: 18rem; border:  solid rgba(0,0,0,.125)"> 
      <div class="card-body m-2">
       <h5 class="card-title">${name}</h5>
-      <p class="card-text">${description} </p>
+      <img src=${image}>
+      <p class="card-text">${description}</p>
       <div class="flex">
           <p style="background-color: rgb(238, 49, 245); margin: 4px; ">${location}</p>
           <p style="background-color: rgb(238, 49, 245); margin: 4px;">${category} </p>
           <p  style="background-color: rgb(238, 49, 245); margin: 4px;">${seniority} </p>
       </div>
-      <button type="button" class="bg-cyan-500 hover:bg-cyan-600 ... p-1 text-slate-50  font-semibold border-double border-2 border-sky-500 rounded " style="margin: 6px;">See Details</button>
+      <button type="button" class="bg-cyan-500 hover:bg-cyan-600 ... p-1 text-slate-50  font-semibold border-double border-2 border-sky-500 rounded id="${id}" style="margin: 6px;">See Details</button>
        <div class="invisible">
-         <button type="button" class="btn bg-green-700 text-black font-normal p-1  rounded " style="margin: 6px;">Edit-job</button>
+         <button type="button" class="btn bg-green-700  hover:bg-green-500  text-black font-normal p-1  rounded " style="margin: 6px;">Edit-job</button>
          <button type="button" class="btn bg-rose-600 text-black font-normal p-1 rounded" style="margin: 6px;">Delete-job</button>
        </div>
     </div> 
@@ -52,10 +55,9 @@ const renderJobs = (data) =>{
      
      `
   }
+  hide("#spinner")
   }
   
-
-
 
 
 
@@ -72,5 +74,6 @@ $("#btn-home").addEventListener("click", () =>{
 
 
  $("#btn-submit").addEventListener("click", () => {
+    saveJob()
 
  })
